5515ef95b65b9045b51b65ea7bf6e311423319eb
monika20060121-ui <monika20060121@gmail.com>
2026-06-23 17:40:21 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.title("Unemployment Analysis in India")
 
 df = pd.read_csv("unemployment in india.csv")
 
-# Clean column names
+# remove extra spaces
 df.columns = df.columns.str.strip()
 
-# Convert date
-df["Date"] = pd.to_datetime(df["Date"], dayfirst=True)
-
+# show data
 st.write(df.head())
 
-plt.figure(figsize=(10,5))
-
-plt.plot(
-    df["Date"].astype(str),
+# line chart
+st.line_chart(
     df["Estimated Unemployment Rate (%)"]
 )
-
-plt.xticks(rotation=45)
-
-st.pyplot(plt)
